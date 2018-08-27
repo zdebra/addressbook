7dec4f94902b37cef8b4a11f6a1bfcdc12776741
zdebra <zd.brabec@gmail.com>
2018-08-27 17:37:30 +0200

    removed doc

--- doc/api_project.js
+++
@@ -1,1 +0,0 @@
-define({  "name": "addressbook",  "version": "1.0.0",  "description": "",  "sampleUrl": false,  "defaultVersion": "0.0.0",  "apidoc": "0.3.0",  "generator": {    "name": "apidoc",    "time": "2018-08-27T15:34:29.618Z",    "url": "http://apidocjs.com",    "version": "0.17.6"  }});
